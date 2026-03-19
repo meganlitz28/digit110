a16735e3e27e8dbf2f893574361ea8ba5c4a970b
meganlitz28 <mjl7107@psu.edu>
2026-03-19 14:04:53 -0400

    testing out the VS code for python code

---
+++ digit210/python-litz/import-nltk-litz.py
@@ -0,0 +1,28 @@
+import nltk
+
+# nltk.download('book')
+
+from nltk.book import *
+from urllib import request
+
+import matplotlib
+import matplotlib.pyplot as plt
+
+#import tk
+#plt.plot(range(10))
+#plt.show()
+
+#text6.dispersion_plot(['rabbit', 'grail','coconut', 'arthur', 'king', 'camelot'])
+#plt.show()
+
+#print(len(set(text6)))
+
+#text1.concordance("monstrous")
+#text1.similar("monstrous")
+#text2.similar("monstrous")
+#text2.common_contexts(["monstrous", "very"])
+
+#text4.dispersion_plot(["citizens", "democracy", "freedom", "duties", "America"])
+#plt.show()
+
+text3.generate()
